2a101d44b72d7abba357487180d56c83d4881f4c
xsfunc <xsfunc@gmail.com>
2023-12-07 21:22:36 +0300

    refactor: typings

--- src/shared/methods.ts
+++ src/shared/methods.ts
@@ -2,9 +2,7 @@ export function random(min: number, max: number): number {
   return Math.floor($fx.rand() * (max - min + 1)) + min
 }
 
-export function sample<T>(array: T[]): T | undefined {
-  if (array.length === 0)
-    return undefined
+export function sample<T>(array: ReadonlyArray<T>): T {
   const randomIndex = Math.floor(Math.random() * array.length)
   return array[randomIndex]
 }
